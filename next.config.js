52287fa153c15c0124594013ee382042180ff996
WJG <wjg12341234@163.com>
2023-03-20 22:05:01 +0800

    misc: add loadEnv function

--- next.config.js
+++ next.config.js
@@ -3,22 +3,29 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const env = {};
-const loaded = dotenv.config({
-  path: `.env.${process.env.APP_ENV || process.env.NODE_ENV}`,
-  silent: true,
-});
-
-env['NEXT_PUBLIC_APP_ENV'] = process.env.APP_ENV;
-
-Object.keys(process.env).forEach((key) => {
-  if (key.startsWith('NEXT_PUBLIC_')) {
-    env[key] = loaded.parsed[key];
-  }
-});
+const loadEnv = () => {
+  const env = {
+    NEXT_PUBLIC_APP_ENV: process.env.APP_ENV || process.env.NODE_ENV,
+  };
+  const loaded = dotenv.config({
+    path: '.env.' + env.NEXT_PUBLIC_APP_ENV,
+    silent: true,
+  });
+  Object.keys(process.env).forEach((key) => {
+    if (key.startsWith('NEXT_PUBLIC_')) {
+      env[key] = loaded.parsed[key];
+    }
+  });
+  return env;
+};
 
 const nextConfig = {
+  env: loadEnv(),
   reactStrictMode: true,
+  images: {
+    disableStaticImages: true,
+    unoptimized: true,
+  },
   webpack: function (config, options) {
     config.resolve.fallback = {
       crypto: false,
@@ -37,14 +44,8 @@ const nextConfig = {
         },
       },
     });
-
     return config;
   },
-  images: {
-    disableStaticImages: true,
-    unoptimized: true,
-  },
-  env,
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
