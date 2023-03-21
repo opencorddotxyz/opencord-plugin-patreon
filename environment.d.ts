86f7fbe829c2c5ca7908a2cba9b5eba4395544c1
zowie <zowietao@gmail.com>
2023-03-21 18:54:27 +0800

    chore: env example

---
+++ environment.d.ts
@@ -0,0 +1,12 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            readonly NEXT_PUBLIC_APP_NAME: string
+            readonly NEXT_PUBLIC_CLIENT_ID: number
+            readonly NEXT_PUBLIC_API_VERSION: string
+            readonly NEXT_PUBLIC_REDIRECT_URI: string
+        }
+    }
+}
+
+export {}
