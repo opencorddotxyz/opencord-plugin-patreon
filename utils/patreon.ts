fb7efe2bd616d30b08c0fa58aa584fa62268106f
zowie <zowietao@gmail.com>
2023-03-21 18:58:33 +0800

    feat: use opencord/client and check localStorage

---
+++ utils/patreon.ts
@@ -0,0 +1,24 @@
+import { format } from 'url';
+
+
+const scopes = [
+  'identity',
+  'identity.memberships',
+  'campaigns',
+  'campaigns.members',
+];
+
+export function getPatreonAuthUrl() {
+  return format({
+    protocol: 'https',
+    host: 'patreon.com',
+    pathname: '/oauth2/authorize',
+    query: {
+      response_type: 'code',
+      client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+      redirect_uri: `${process.env.NEXT_PUBLIC_REDIRECT_URI}/oauth`,
+      scope: scopes.join(' '),
+      state: 'chill',
+    },
+  });
+}
