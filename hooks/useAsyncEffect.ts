2658780dadfd8940882559abd3018d6f922d7dbd
zowie <zowietao@gmail.com>
2023-03-21 18:56:05 +0800

    feat: add useAsyncEffect hooks

---
+++ hooks/useAsyncEffect.ts
@@ -0,0 +1,39 @@
+import type { DependencyList } from 'react';
+import { useEffect } from 'react';
+
+import { isFunction } from '@/utils/core/is';
+
+function useAsyncEffect(
+  effect: () => AsyncGenerator<void, void, void> | Promise<void>,
+  deps?: DependencyList,
+) {
+  function isAsyncGenerator(
+    val: AsyncGenerator<void, void, void> | Promise<void>,
+  ): val is AsyncGenerator<void, void, void> {
+    return isFunction(val[Symbol.asyncIterator]);
+  }
+  useEffect(() => {
+    const e = effect();
+    let cancelled = false;
+    async function execute() {
+      if (isAsyncGenerator(e)) {
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+          const result = await e.next();
+          if (result.done || cancelled) {
+            break;
+          }
+        }
+      } else {
+        await e;
+      }
+    }
+    execute();
+    return () => {
+      cancelled = true;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
+}
+
+export default useAsyncEffect;
