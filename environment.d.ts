f16a4af9c4fc54046743167d1703271e102b5354
zowie <zowietao@gmail.com>
2023-03-23 17:58:20 +0800

    chore: env rename

--- environment.d.ts
+++ environment.d.ts
@@ -4,7 +4,7 @@ declare global {
             readonly NEXT_PUBLIC_APP_NAME: string
             readonly NEXT_PUBLIC_CLIENT_ID: number
             readonly NEXT_PUBLIC_API_VERSION: string
-            readonly NEXT_PUBLIC_REDIRECT_URI: string
+            readonly NEXT_PUBLIC_APP_URI: string
             readonly NEXT_PUBLIC_OC_APP_SITE: string
         }
     }
