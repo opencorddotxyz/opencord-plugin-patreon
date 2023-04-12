5e497f2e377e1309ffacb53d70de9ed44f654059
zowie <zowietao@gmail.com>
2023-04-12 16:20:38 +0800

    chore: merge zowie branch and modify import

--- hooks/useConnectPatreon.ts
+++ hooks/useConnectPatreon.ts
@@ -3,23 +3,32 @@ import { useState } from 'react';
 import { openNewTab } from '@/utils/location';
 import { getPatreonAuthUrl } from '@/utils/patreon';
 
+import { useBreakpoint } from './core/useBreakpoint';
 import { useAPP } from './useAPP';
 
 export const useConnectPatreon = () => {
   const [connecting, setConnecting] = useState(false);
+  const { isMobile } = useBreakpoint();
   const { refreshHomeStates } = useAPP();
 
   const connectPatreon = async () => {
+    const oauthURL = getPatreonAuthUrl();
     if (connecting) {
       return;
     }
+    isMobile && openNewTab(oauthURL);
+
     setConnecting(true);
-    const states = await refreshHomeStates();
-    setConnecting(false);
-    const isConnected = states?.connected;
-    if (!isConnected) {
-      const oauthURL = getPatreonAuthUrl();
-      openNewTab(oauthURL);
+    try {
+      const states = await refreshHomeStates();
+      const isConnected = states?.connected;
+      if (!isConnected) {
+        openNewTab(oauthURL);
+      }
+    } catch (error) {
+      //
+    } finally {
+      setConnecting(false);
     }
   };
 
